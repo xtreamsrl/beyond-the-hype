370f4146a1916998ab0edef74fe738f6cb1124eb
Fabio Lipreri <fabio.lipreri@xtreamers.io>
2025-01-21 12:20:45 +0100

    feat: add first naive rag notebook (#5)
    * build: add openai and python-dotenv as project dependency
    
    * build: add pywidget as dependency to remove sentence-transformer tqdm warnings in notebooks
    
    * feat: add data utils to import movies dataset
    
    * feat: add function to easily imports openai api key
    
    * feat: add notebook containing the first version of rag

---
+++ src/ace_of_splades/data.py
@@ -0,0 +1,29 @@
+"""Provide functions for data imports and management."""
+
+from pathlib import Path
+
+import polars as pl
+
+
+def get_movies_dataset(sample: int = 0, *, local: bool = False) -> pl.DataFrame:
+    """Retrieve a dataset of movies with embeddings vectors.
+
+    Args:
+        sample: number of rows to sample from the dataset.
+        local: whether to load the dataset from a local file or from the web.
+
+    Returns:
+        A dataset of movies with their plots.
+
+    """
+    file_name = "movies_plots_dataset_embd_minilm.parquet"
+    if local:
+        here = Path(__file__)
+        source = here.parent.parent.parent / f"data/{file_name}"
+    else:
+        source = f"https://raw.githubusercontent.com/xtreamsrl/ace-of-splades/main/data/{file_name}"
+
+    if sample:
+        return pl.read_parquet(source).sample(sample)
+
+    return pl.read_parquet(source)
